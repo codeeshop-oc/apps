2d1d54548dd73cd203d80608e56d6b8d6fe7c2f3
codeeshop-oc <anantnegi8@gmail.com>
2023-10-11 16:15:31 +0530

    update

---
+++ template_change.sh
@@ -0,0 +1,6 @@
+sed -i "s/{{ APP_NAME }}//g" _config.yml
+sed -i "s/{{ IOS_APP_ID }}//g" _config.yml
+sed -i "s/{{ IOS_APP_COUNTRY }}//g" _config.yml
+sed -i "s/{{ DOWNLOADABLE_FILE_LINK }}//g" _config.yml
+sed -i "s/{{ APP_TAGLINE }}//g" _config.yml
+sed -i "s/{{ APP_SMARTBANNER }}/true/g" _config.yml
